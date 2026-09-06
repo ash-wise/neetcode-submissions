75e812b2c4928bed04d3441884b17f70ca7e5c45
ash-wise <ash.var07@gmail.com>
2026-09-06 01:41:47 +0100

    Bulk sync: 2 submissions

---
+++ Data Structures & Algorithms/is-anagram/submission-0.py
@@ -0,0 +1,17 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        counts={}
+        if len(s) != len(t):
+            return False
+        for ch in s:
+            if ch not in counts:
+                    counts[ch] = 1
+            else:
+                    counts[ch] += 1
+        for ch in t:
+            if ch not in counts:
+                return False
+                counts[ch] -= 1
+            if counts[ch] < 0:
+                return False
+        return True
